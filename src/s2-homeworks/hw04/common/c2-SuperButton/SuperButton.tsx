dc25bc0dc5c5c34e51e38867365186b493b749c6
Alexandr Dylevich <dylevich220885@gmail.com>
2023-02-21 00:20:52 +0300

    fixed 'finalClassName' in the 'SuperButton' classes to the button

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -18,6 +18,13 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
+        + (disabled
+            ? ' ' + s.disabled
+            : xType === 'red'
+                ? ' ' + s.red
+                : xType === 'secondary'
+                    ? ' ' + s.secondary
+                    :' ' + s.default)
         // + (disabled
         //         ? ...
         //         : xType === 'red'
