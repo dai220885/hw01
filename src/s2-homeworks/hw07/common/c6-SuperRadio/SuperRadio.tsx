8fdf8c3963a19f95b3461cadaacad1839488c632
Alexandr Dylevich <dylevich220885@gmail.com>
2023-03-14 23:30:14 +0300

    (HW7)

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -36,6 +36,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
 }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // делают студенты
+        onChangeOption&&onChangeOption(parseInt(e.currentTarget.value))
     }
 
     const finalRadioClassName = s.radio + (className ? ' ' + className : '')
@@ -49,6 +50,10 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       className={finalRadioClassName}
                       type={'radio'}
                       // name, checked, value делают студенты
+                      name ={o.name}
+                      value={o.id}
+                      //checked={o.id===value}
+                      checked={value===o.id}
 
                       onChange={onChangeCallback}
                       {...restProps}
